6494ff7c3ed3b153a424c1f6c91d940f9b3a4d44
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-04-17 22:29:07 +0600

    optimise duplicate code

--- src/Pages/Login/SignUp/SignUp.jsx
+++ src/Pages/Login/SignUp/SignUp.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 import register from "../img/register_bg_2.png";
-import github from "../img/github.svg";
-import google from "../img/google.svg";
 import { Link } from "react-router-dom";
 import {
   useCreateUserWithEmailAndPassword,
@@ -9,6 +7,7 @@ import {
   useSendEmailVerification,
 } from "react-firebase-hooks/auth";
 import auth from "../../../Firebase.init";
+import SocialLogIn from "../SocialLogIn/SocialLogIn";
 
 const SignUp = () => {
   const [createUserWithEmailAndPassword, user, loading, errorCreateUser] =
@@ -47,32 +46,7 @@ const SignUp = () => {
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-              <div className="rounded-t mb-0 px-6 py-6">
-                <div className="text-center mb-3">
-                  <h6 className="text-gray-600 text-sm font-bold">
-                    Sign up with
-                  </h6>
-                </div>
-                <div className="btn-wrapper text-center">
-                  <button
-                    className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center hover:font-bold text-xs"
-                    type="button"
-                    style={{ transition: "all .15s ease" }}
-                  >
-                    <img alt="..." className="w-5 mr-1" src={github} />
-                    Github
-                  </button>
-                  <button
-                    className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center hover:font-bold text-xs"
-                    type="button"
-                    style={{ transition: "all .15s ease" }}
-                  >
-                    <img alt="..." className="w-5 mr-1" src={google} />
-                    Google
-                  </button>
-                </div>
-                <hr className="mt-6 border-b-1 border-gray-400" />
-              </div>
+              <SocialLogIn type="Sign up" />
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                 <div className="text-gray-500 text-center mb-3 font-bold">
                   <small>Or Sign up with credentials</small>
