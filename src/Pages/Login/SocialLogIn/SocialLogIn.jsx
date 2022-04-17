063ef616b3f07621008122e823e8336d96f0f309
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-04-17 22:54:05 +0600

    add signinWithGithub functionality

--- src/Pages/Login/SocialLogIn/SocialLogIn.jsx
+++ src/Pages/Login/SocialLogIn/SocialLogIn.jsx
@@ -1,11 +1,17 @@
 import React from "react";
-import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import {
+  useSignInWithGithub,
+  useSignInWithGoogle,
+} from "react-firebase-hooks/auth";
 import auth from "../../../Firebase.init";
 import github from "../img/github.svg";
 import google from "../img/google.svg";
 
 const SocialLogIn = ({ type }) => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, userGoogle, loadingGoogle, errorGoogle] =
+    useSignInWithGoogle(auth);
+  const [signInWithGithub, userGithub, loadingGithub, errorGithub] =
+    useSignInWithGithub(auth);
 
   return (
     <div className="rounded-t mb-0 px-6 py-6">
@@ -17,6 +23,7 @@ const SocialLogIn = ({ type }) => {
           className="bg-white active:bg-gray-100 text-gray-800 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center hover:font-bold text-xs"
           type="button"
           style={{ transition: "all .15s ease" }}
+          onClick={async () => await signInWithGithub()}
         >
           <img alt="..." className="w-5 mr-1" src={github} />
           Github
