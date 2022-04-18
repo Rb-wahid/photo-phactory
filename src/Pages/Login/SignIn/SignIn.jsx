25caf5eb4807ce649128a8c7c4441e4e2ec86c7b
Raihan Bin Wahid <cse.rbwahid@gmail.com>
2022-04-18 17:28:20 +0600

    add spinner

--- src/Pages/Login/SignIn/SignIn.jsx
+++ src/Pages/Login/SignIn/SignIn.jsx
@@ -9,6 +9,7 @@ import {
   useSignInWithEmailAndPassword,
   useSendPasswordResetEmail,
 } from "react-firebase-hooks/auth";
+import Spinner from "../../Spinner/Spinner";
 
 export default function SignIn() {
   let navigate = useNavigate();
@@ -27,6 +28,7 @@ export default function SignIn() {
     let err = errorSignIn || errorResetPassword;
     setError(err);
   }
+  const loader = loading || sending;
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -60,90 +62,96 @@ export default function SignIn() {
 
   return (
     <section className="absolute w-full h-full">
-      <div
-        className="absolute top-0 w-full h-full bg-gray-900"
-        style={{
-          backgroundImage: `url(${register})`,
-          backgroundSize: "100%",
-          backgroundRepeat: "no-repeat",
-        }}
-      ></div>
-      <div className="container mx-auto px-4 h-full">
-        <div className="flex content-center items-center justify-center h-full">
-          <div className="w-full lg:w-4/12 px-4">
-            <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
-              <SocialLogIn type="Sign in" />
-              <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                <div className="text-gray-500 text-center mb-3 font-bold">
-                  <small>Or sign in with credentials</small>
-                </div>
-                <form onSubmit={handleSubmit}>
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="email"
-                    >
-                      Email
-                    </label>
-                    <input
-                      onChange={(e) => setEmail(e.currentTarget.value)}
-                      type="email"
-                      className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                      placeholder="Email"
-                      style={{ transition: "all .15s ease" }}
-                      id="email"
-                      required
-                    />
-                  </div>
+      {loader ? (
+        <Spinner />
+      ) : (
+        <>
+          <div
+            className="absolute top-0 w-full h-full bg-gray-900"
+            style={{
+              backgroundImage: `url(${register})`,
+              backgroundSize: "100%",
+              backgroundRepeat: "no-repeat",
+            }}
+          ></div>
+          <div className="container mx-auto px-4 h-full">
+            <div className="flex content-center items-center justify-center h-full">
+              <div className="w-full lg:w-4/12 px-4">
+                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300 border-0">
+                  <SocialLogIn type="Sign in" />
+                  <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+                    <div className="text-gray-500 text-center mb-3 font-bold">
+                      <small>Or sign in with credentials</small>
+                    </div>
+                    <form onSubmit={handleSubmit}>
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                          htmlFor="email"
+                        >
+                          Email
+                        </label>
+                        <input
+                          onChange={(e) => setEmail(e.currentTarget.value)}
+                          type="email"
+                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                          placeholder="Email"
+                          style={{ transition: "all .15s ease" }}
+                          id="email"
+                          required
+                        />
+                      </div>
 
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="password"
-                    >
-                      Password
-                    </label>
-                    <input
-                      type="password"
-                      className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
-                      placeholder="Password"
-                      style={{ transition: "all .15s ease" }}
-                      id="password"
-                      required
-                    />
+                      <div className="relative w-full mb-3">
+                        <label
+                          className="block uppercase text-gray-700 text-xs font-bold mb-2"
+                          htmlFor="password"
+                        >
+                          Password
+                        </label>
+                        <input
+                          type="password"
+                          className="border-0 px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full"
+                          placeholder="Password"
+                          style={{ transition: "all .15s ease" }}
+                          id="password"
+                          required
+                        />
+                      </div>
+                      {error && (
+                        <div className="text-center mt-6 text-red-700 font-semibold">
+                          <p style={{ transition: "all .15s ease" }}>
+                            {error.message}
+                          </p>
+                        </div>
+                      )}
+                      <div className="text-center mt-6">
+                        <button
+                          className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
+                          type="submit"
+                          style={{ transition: "all .15s ease" }}
+                        >
+                          Sign In
+                        </button>
+                      </div>
+                      <div className="flex flex-wrap mt-6">
+                        <div className="w-1/2 text-blue-500 hover:text-blue-700 font-semibold">
+                          <Link to onClick={resetPassword}>
+                            Forgot password?
+                          </Link>
+                        </div>
+                        <div className="w-1/2 text-right text-blue-500 hover:text-blue-700 font-semibold">
+                          <Link to="/signup">Create new account</Link>
+                        </div>
+                      </div>
+                    </form>
                   </div>
-                  {error && (
-                    <div className="text-center mt-6 text-red-700 font-semibold">
-                      <p style={{ transition: "all .15s ease" }}>
-                        {error.message}
-                      </p>
-                    </div>
-                  )}
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full"
-                      type="submit"
-                      style={{ transition: "all .15s ease" }}
-                    >
-                      Sign In
-                    </button>
-                  </div>
-                  <div className="flex flex-wrap mt-6">
-                    <div className="w-1/2 text-blue-500 hover:text-blue-700 font-semibold">
-                      <Link to onClick={resetPassword}>
-                        Forgot password?
-                      </Link>
-                    </div>
-                    <div className="w-1/2 text-right text-blue-500 hover:text-blue-700 font-semibold">
-                      <Link to="/signup">Create new account</Link>
-                    </div>
-                  </div>
-                </form>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </div>
+        </>
+      )}
       <ToastContainer />
     </section>
   );
